65bbbff57fa8242b7813c91c35c4966712fefea8
klasing <klasing1959@gmail.com>
2017-11-09 21:29:26 +0100

    09-11-2017 21:28 S03-02

--- Sunshine/app/src/main/java/com/example/sunshine/ForecastAdapter.java
+++ Sunshine/app/src/main/java/com/example/sunshine/ForecastAdapter.java
@@ -4,22 +4,38 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
     private String[] mWeatherData;
 
-    public ForecastAdapter() {
+    private final ForecastAdapterOnClickHandler mClickHandler;
 
+    public interface ForecastAdapterOnClickHandler {
+        void onClick(String weatherForDay);
     }
 
-    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+    public ForecastAdapter(ForecastAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
         public final TextView mWeatherTextView;
 
         public ForecastAdapterViewHolder(View view) {
             super(view);
             mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            String weatherForDay = mWeatherData[adapterPosition];
+            mClickHandler.onClick(weatherForDay);
         }
     }
 
