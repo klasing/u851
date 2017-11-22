924ec3ed8248b75c908059a9f0467650d0a2d7ee
klasing <klasing1959@gmail.com>
2017-11-22 10:11:32 +0100

    22-11-2017 10:10 S10-03 Did not wait for SYNC_INTERVAL of n HOURS

--- Sunshine/app/src/main/java/com/example/sunshine/sync/SunshineSyncUtils.java
+++ Sunshine/app/src/main/java/com/example/sunshine/sync/SunshineSyncUtils.java
@@ -4,13 +4,55 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 import com.example.sunshine.data.WeatherContract;
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
+import java.util.concurrent.TimeUnit;
 
 public class SunshineSyncUtils {
+    private static final int SYNC_INTERVAL_HOURS = 3;
+    private static final int SYNC_INTERVAL_SECONDS = (int) TimeUnit.HOURS.toSeconds(SYNC_INTERVAL_HOURS);
+    private static final int SYNC_FLEXTIME_SECONDS = SYNC_INTERVAL_SECONDS / 3;
+
     private static boolean sInitialized;
+
+    private static final String SUNSHINE_SYNC_TAG = "sunshine-sync";
+
+    static void scheduleFirebaseJobDispatcherSync(@NonNull final Context context) {
+
+        Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
+
+        Job syncSunshineJob = dispatcher.newJobBuilder()
+
+                .setService(SunshineFirebaseJobService.class)
+
+                .setTag(SUNSHINE_SYNC_TAG)
+
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+
+                .setLifetime(Lifetime.FOREVER)
+
+                .setRecurring(true)
+
+                .setTrigger(Trigger.executionWindow(
+                        SYNC_INTERVAL_SECONDS,
+                        SYNC_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS))
+
+                .setReplaceCurrent(true)
+
+                .build();
+
+        dispatcher.schedule(syncSunshineJob);
+    }
 
     synchronized public static void initialize(@NonNull final Context context) {
 
@@ -18,9 +60,36 @@ public class SunshineSyncUtils {
 
         sInitialized = true;
 
-        new AsyncTask<Void, Void, Void>() {
+        scheduleFirebaseJobDispatcherSync(context);
+
+//        new AsyncTask<Void, Void, Void>() {
+//            @Override
+//            public Void doInBackground( Void... voids ) {
+//
+//                Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
+//
+//                String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+//                String selectionStatement = WeatherContract.WeatherEntry
+//                        .getSqlSelectForTodayOnwards();
+//
+//                Cursor cursor = context.getContentResolver().query(
+//                        forecastQueryUri,
+//                        projectionColumns,
+//                        selectionStatement,
+//                        null,
+//                        null);
+//                if (null == cursor || cursor.getCount() == 0) {
+//                    startImmediateSync(context);
+//                }
+//
+//                cursor.close();
+//                return null;
+//            }
+//        }.execute();
+
+        Thread checkForEmpty = new Thread(new Runnable() {
             @Override
-            public Void doInBackground( Void... voids ) {
+            public void run() {
 
                 Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
 
@@ -34,14 +103,16 @@ public class SunshineSyncUtils {
                         selectionStatement,
                         null,
                         null);
+
                 if (null == cursor || cursor.getCount() == 0) {
                     startImmediateSync(context);
                 }
 
                 cursor.close();
-                return null;
             }
-        }.execute();
+        });
+
+        checkForEmpty.start();
     }
 
     public static void startImmediateSync(@NonNull final Context context) {
